ec25a9680e4f19eea1a84f8a659d5f824c6aed3d
Artem Dmysh <artemmkoko2@gmail.com>
2024-02-25 20:27:05 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let intervalTimer;
-let seconds = 0
-let minutes = 0
-let hours = 0
+const fiveMinutes = document.getElementById('fiveMinutes')
 
+let intervalTimer;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let isTimerRunning = false;
+let activeStartbtn = false;
+ 
 function updateTimer() {
   seconds++
+
   if (seconds >= 60) {
     seconds = 0
     minutes++
@@ -17,12 +22,19 @@ function updateTimer() {
   const formattedTimer = 
   (hours < 10 ? '0' + hours : hours) + ':' +
   (minutes < 10 ? '0' + minutes : minutes) + ':' +
-  (seconds < 10 ? '0' + seconds : seconds)
+  (seconds < 10 ? '0' + seconds : seconds) 
   document.getElementById('timer').textContent = formattedTimer
 }
-document.getElementById('startBtn').addEventListener('click',function() {
-  clearInterval(intervalTimer)
-  intervalTimer = setInterval(updateTimer,1000)
+
+document.getElementById('startBtn').addEventListener('click', function() {
+    clearInterval(intervalTimer)
+    intervalTimer = setInterval(updateTimer, 1000)
+
+    if (!isTimerRunning && !activeStartbtn) {
+      clearInterval(intervalTimer);
+      intervalTimer = setInterval(updateTimer, 1000);
+      activeStartbtn = true;
+  }
 })
 document.getElementById('stopBtn').addEventListener('click',function() {
   clearInterval(intervalTimer)
@@ -30,5 +42,58 @@ document.getElementById('stopBtn').addEventListener('click',function() {
 document.getElementById('clearBtn').addEventListener('click',function() {
   clearInterval(intervalTimer)
   hours = minutes = seconds = 0
-  document.getElementById('timer').textContent = '00:00:00'
+  document.getElementById('timer').textContent = '00:00:00'  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======
+document.getElementById('fiveMinutes').addEventListener('click',function() {
+  function fiveMinutesTimer(timerElement) {
+    let timer = timerElement
+
+    function activeFiveMinutes() {
+      const minutes = Math.floor(timer / 60)
+      const seconds = timer % 60
+
+      document.getElementById('timer').textContent = `${minutes}:${seconds}`
+      
+      const formattedTime = 
+      (hours < 10 ? '0' + hours : hours) + ':' +
+      (minutes < 10 ? '0' + minutes : minutes) + ':' +
+      (seconds < 10 ? '0' + seconds : seconds) 
+      document.getElementById('timer').textContent = formattedTime
+
+      if (--timer < 0) {
+      document.getElementById('timer').textContent = 'timer is over'   
+      } else {
+        setTimeout(activeFiveMinutes, 1000)
+      }
+    }
+    activeFiveMinutes()
+    
+  }
+  fiveMinutesTimer(5 * 60)
+})
+
